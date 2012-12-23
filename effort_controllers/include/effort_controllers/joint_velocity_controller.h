c003ba78fd6cd7d1a17ca90dc417d9d74ea274e0
Wim Meeussen <wim@samsung.(none)>
2012-12-23 12:40:33 -0800

    port another controller

---
+++ effort_controllers/include/effort_controllers/joint_velocity_controller.h
@@ -0,0 +1,127 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2008, Willow Garage, Inc.
+ *  Copyright (c) 2012, hiDOF, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+#ifndef EFFORT_CONTROLLERS_JOINT_VELOCITY_CONTROLLER_H
+#define EFFORT_CONTROLLERS_JOINT_VELOCITY_CONTROLLER_H
+
+/**
+   @class velocity_controllers:JointVelocityController
+   @brief Joint Velocity Controller
+
+   This class passes the commanded velocity down to the joint
+
+   @section ROS interface
+
+   @param type Must be "JointVelocityController"
+   @param joint Name of the joint to control.
+
+   Subscribes to:
+   - @b command (std_msgs::Float64) : The joint velocity to apply
+*/
+
+
+#include <boost/thread/condition.hpp>
+#include <ros/node_handle.h>
+#include <boost/scoped_ptr.hpp>
+#include <hardware_interface/joint_command_interface.h>
+#include <controller_interface/controller.h>
+#include <controllers_msgs/JointControllerState.h>
+#include <std_msgs/Float64.h>
+#include <control_toolbox/pid.h>
+#include <control_toolbox/pid_gains_setter.h>
+#include <realtime_tools/realtime_publisher.h>
+
+namespace effort_controllers
+{
+
+class JointVelocityController: public controller_interface::Controller<hardware_interface::EffortJointInterface>
+{
+public:
+
+  JointVelocityController();
+  ~JointVelocityController();
+
+  bool init(hardware_interface::EffortJointInterface *robot, const std::string &joint_name, const control_toolbox::Pid &pid);
+  bool init(hardware_interface::EffortJointInterface *robot, ros::NodeHandle &n);
+
+  /*!
+   * \brief Give set position of the joint for next update: revolute (angle) and prismatic (position)
+   *
+   * \param double pos Velocity command to issue
+   */
+  void setCommand(double cmd);
+
+  /*!
+   * \brief Get latest position command to the joint: revolute (angle) and prismatic (position).
+   */
+  void getCommand(double & cmd);
+
+  /*!
+   * \brief Issues commands to the joint. Should be called at regular intervals
+   */
+
+  void starting(const ros::Time& time) {
+    last_time_ = time;
+    command_ = 0.0;
+    pid_controller_.reset();
+  }
+  void update(const ros::Time& time);
+
+  void getGains(double &p, double &i, double &d, double &i_max, double &i_min);
+  void setGains(const double &p, const double &i, const double &d, const double &i_max, const double &i_min);
+
+  std::string getJointName();
+  hardware_interface::JointHandle joint_;
+
+  double command_;                                /**< Last commanded position. */
+
+private:
+  control_toolbox::Pid pid_controller_;           /**< Internal PID controller. */
+  ros::Time last_time_;                              /**< Last time stamp of update. */
+  int loop_count_;
+
+  friend class JointVelocityControllerNode;
+
+  boost::scoped_ptr<
+    realtime_tools::RealtimePublisher<
+      controllers_msgs::JointControllerState> > controller_state_publisher_ ;
+
+  ros::Subscriber sub_command_;
+  void setCommandCB(const std_msgs::Float64ConstPtr& msg);
+};
+
+} // namespace
+
+#endif
---
+++ effort_controllers/src/joint_velocity_controller.cpp
@@ -0,0 +1,147 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2008, Willow Garage, Inc.
+ *  Copyright (c) 2012, hiDOF, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+#include <effort_controllers/joint_velocity_controller.h>
+#include <pluginlib/class_list_macros.h>
+
+
+namespace effort_controllers {
+
+JointVelocityController::JointVelocityController()
+: command_(0), last_time_(0), loop_count_(0)
+{}
+
+JointVelocityController::~JointVelocityController()
+{
+  sub_command_.shutdown();
+}
+
+  bool JointVelocityController::init(hardware_interface::EffortJointInterface *robot, 
+				     const std::string &joint_name, const control_toolbox::Pid &pid)
+{
+  joint_ = robot->getJointHandle(joint_name);
+  pid_controller_ = pid;
+
+  return true;
+}
+
+  bool JointVelocityController::init(hardware_interface::EffortJointInterface *robot, ros::NodeHandle &n)
+{
+  std::string joint_name;
+  if (!n.getParam("joint", joint_name)) {
+    ROS_ERROR("No joint given (namespace: %s)", n.getNamespace().c_str());
+    return false;
+  }
+
+  joint_ = robot->getJointHandle(joint_name);
+
+  if (!pid_controller_.init(ros::NodeHandle(n, "pid")))
+    return false;
+
+  controller_state_publisher_.reset(
+    new realtime_tools::RealtimePublisher<controllers_msgs::JointControllerState>
+    (n, "state", 1));
+
+  sub_command_ = n.subscribe<std_msgs::Float64>("command", 1, &JointVelocityController::setCommandCB, this);
+
+  return true;
+}
+
+
+void JointVelocityController::setGains(const double &p, const double &i, const double &d, const double &i_max, const double &i_min)
+{
+  pid_controller_.setGains(p,i,d,i_max,i_min);
+
+}
+
+void JointVelocityController::getGains(double &p, double &i, double &d, double &i_max, double &i_min)
+{
+  pid_controller_.getGains(p,i,d,i_max,i_min);
+}
+
+std::string JointVelocityController::getJointName()
+{
+  return joint_.getName();
+}
+
+// Set the joint velocity command
+void JointVelocityController::setCommand(double cmd)
+{
+  command_ = cmd;
+}
+
+// Return the current velocity command
+void JointVelocityController::getCommand(double  & cmd)
+{
+  cmd = command_;
+}
+
+  void JointVelocityController::update(const ros::Time& time)
+{
+  ros::Duration dt = time - last_time_;
+  double error = joint_.getVelocity() - command_;
+  double command = pid_controller_.updatePid(error, dt);
+  joint_.setCommand(command);
+
+  if(loop_count_ % 10 == 0)
+  {
+    if(controller_state_publisher_ && controller_state_publisher_->trylock())
+    {
+      controller_state_publisher_->msg_.header.stamp = time;
+      controller_state_publisher_->msg_.set_point = command_;
+      controller_state_publisher_->msg_.process_value = joint_.getVelocity();
+      controller_state_publisher_->msg_.error = error;
+      controller_state_publisher_->msg_.time_step = dt.toSec();
+      controller_state_publisher_->msg_.command = command;
+
+      double dummy;
+      getGains(controller_state_publisher_->msg_.p,
+               controller_state_publisher_->msg_.i,
+               controller_state_publisher_->msg_.d,
+               controller_state_publisher_->msg_.i_clamp,
+               dummy);
+      controller_state_publisher_->unlockAndPublish();
+    }
+  }
+  loop_count_++;
+  last_time_ = time;
+}
+
+void JointVelocityController::setCommandCB(const std_msgs::Float64ConstPtr& msg)
+{
+  command_ = msg->data;
+}
+
+} // namespace

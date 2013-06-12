9786e9a8d15266378da60675ce906e58882f7eda
Adolfo Rodriguez Tsouroukdissian <adolfo.rodriguez@pal-robotics.com>
2013-06-12 18:23:16 +0200

    Controller publishing the state of a IMU sensor.

---
+++ imu_sensor_controller/src/imu_sensor_controller.cpp
@@ -0,0 +1,196 @@
+///////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2012, hiDOF INC.
+// Copyright (C) 2013, PAL Robotics S.L.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//   * Redistributions of source code must retain the above copyright notice,
+//     this list of conditions and the following disclaimer.
+//   * Redistributions in binary form must reproduce the above copyright
+//     notice, this list of conditions and the following disclaimer in the
+//     documentation and/or other materials provided with the distribution.
+//   * Neither the name of hiDOF, Inc. nor the names of its
+//     contributors may be used to endorse or promote products derived from
+//     this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//////////////////////////////////////////////////////////////////////////////
+
+/// \author: Adolfo Rodriguez Tsouroukdissian
+
+
+#include "imu_sensor_controller/imu_sensor_controller.h"
+
+
+
+namespace imu_sensor_controller
+{
+
+  bool ImuSensorController::init(hardware_interface::ImuSensorInterface* hw, ros::NodeHandle &root_nh, ros::NodeHandle& controller_nh)
+  {
+    // get all joint states from the hardware interface
+    const std::vector<std::string>& sensor_names = hw->getSensorNames();
+    for (unsigned i=0; i<sensor_names.size(); i++)
+      ROS_DEBUG("Got sensor %s", sensor_names[i].c_str());
+
+    // get publishing period
+    if (!controller_nh.getParam("publish_rate", publish_rate_)){
+      ROS_ERROR("Parameter 'publish_rate' not set");
+      return false;
+    }
+
+    for (unsigned i=0; i<sensor_names.size(); i++){
+      // sensor handle
+      sensors_.push_back(hw->getSensorHandle(sensor_names[i]));
+
+      // realtime publisher
+      RtPublisherPtr rt_pub(new realtime_tools::RealtimePublisher<sensor_msgs::Imu>(root_nh, sensor_names[i], 4));
+      realtime_pubs_.push_back(rt_pub);
+    }
+
+    // Last published times
+    last_publish_times_.resize(sensor_names.size());
+    return true;
+  }
+
+  void ImuSensorController::starting(const ros::Time& time)
+  {
+    // initialize time
+    for (unsigned i=0; i<last_publish_times_.size(); i++){
+      last_publish_times_[i] = time;
+    }
+  }
+
+  void ImuSensorController::update(const ros::Time& time, const ros::Duration& period)
+  {
+    using namespace hardware_interface;
+
+    // limit rate of publishing
+    for (unsigned i=0; i<realtime_pubs_.size(); i++){
+      if (publish_rate_ > 0.0 && last_publish_times_[i] + ros::Duration(1.0/publish_rate_) < time){
+        // try to publish
+        if (realtime_pubs_[i]->trylock()){
+          // we're actually publishing, so increment time
+          last_publish_times_[i] = last_publish_times_[i] + ros::Duration(1.0/publish_rate_);
+
+          // populate message
+          realtime_pubs_[i]->msg_.header.stamp = time;
+          realtime_pubs_[i]->msg_.header.frame_id = sensors_[i].getFrameId();
+
+          // Orientation
+          if (sensors_[i].getCapabilities() & ImuSensorHandle::ORIENTATION)
+          {
+            realtime_pubs_[i]->msg_.orientation.x = sensors_[i].getOrientation().x();
+            realtime_pubs_[i]->msg_.orientation.y = sensors_[i].getOrientation().y();
+            realtime_pubs_[i]->msg_.orientation.z = sensors_[i].getOrientation().z();
+            realtime_pubs_[i]->msg_.orientation.w = sensors_[i].getOrientation().w();
+          }
+
+          // Orientation covariance
+          if (sensors_[i].getCapabilities() & ImuSensorHandle::ORIENTATION_COVARIANCE)
+          {
+            for (unsigned j=0; j<realtime_pubs_[i]->msg_.orientation_covariance.size(); ++j){
+              realtime_pubs_[i]->msg_.orientation_covariance[i] = sensors_[i].getOrientationCovariance()(i);
+            }
+          }
+          else
+          {
+            if (sensors_[i].getCapabilities() & ImuSensorHandle::ORIENTATION)
+            {
+              // Orientation available
+              for (unsigned j=0; j<realtime_pubs_[i]->msg_.orientation_covariance.size(); ++j){
+                realtime_pubs_[i]->msg_.orientation_covariance[j] = 0.0;
+              }
+            }
+            else
+            {
+              // No orientation available
+              realtime_pubs_[i]->msg_.orientation_covariance[0] = -1.0;
+            }
+          }
+
+          // Angular velocity
+          if (sensors_[i].getCapabilities() & ImuSensorHandle::ANGULAR_VELOCITY)
+          {
+            realtime_pubs_[i]->msg_.angular_velocity.x = sensors_[i].getAngularVelocity().x();
+            realtime_pubs_[i]->msg_.angular_velocity.y = sensors_[i].getAngularVelocity().y();
+            realtime_pubs_[i]->msg_.angular_velocity.z = sensors_[i].getAngularVelocity().z();
+          }
+
+          // Angular velocity covariance
+          if (sensors_[i].getCapabilities() & ImuSensorHandle::ANGULAR_VELOCITY_COVARIANCE)
+          {
+            for (unsigned j=0; j<realtime_pubs_[i]->msg_.angular_velocity_covariance.size(); ++j){
+              realtime_pubs_[i]->msg_.angular_velocity_covariance[i] = sensors_[i].getAngularVelocityCovariance()(i);
+            }
+          }
+          else
+          {
+            if (sensors_[i].getCapabilities() & ImuSensorHandle::ANGULAR_VELOCITY)
+            {
+              // Angular velocity available
+              for (unsigned j=0; j<realtime_pubs_[i]->msg_.angular_velocity_covariance.size(); ++j){
+                realtime_pubs_[i]->msg_.angular_velocity_covariance[j] = 0.0;
+              }
+            }
+            else
+            {
+              // No angular velocity available
+              realtime_pubs_[i]->msg_.angular_velocity_covariance[0] = -1.0;
+            }
+          }
+
+          // Linear acceleration
+          if (sensors_[i].getCapabilities() & ImuSensorHandle::LINEAR_ACCELERATION)
+          {
+            realtime_pubs_[i]->msg_.linear_acceleration.x = sensors_[i].getLinearAcceleration().x();
+            realtime_pubs_[i]->msg_.linear_acceleration.y = sensors_[i].getLinearAcceleration().y();
+            realtime_pubs_[i]->msg_.linear_acceleration.z = sensors_[i].getLinearAcceleration().z();
+          }
+
+          // Linear acceleration covariance
+          if (sensors_[i].getCapabilities() & ImuSensorHandle::LINEAR_ACCELERATION_COVARIANCE)
+          {
+            for (unsigned j=0; j<realtime_pubs_[i]->msg_.linear_acceleration_covariance.size(); ++j){
+              realtime_pubs_[i]->msg_.linear_acceleration_covariance[i] = sensors_[i].getLinearAccelerationCovariance()(i);
+            }
+          }
+          else
+          {
+            if (sensors_[i].getCapabilities() & ImuSensorHandle::LINEAR_ACCELERATION)
+            {
+              // Linear acceleration available
+              for (unsigned j=0; j<realtime_pubs_[i]->msg_.linear_acceleration_covariance.size(); ++j){
+                realtime_pubs_[i]->msg_.linear_acceleration_covariance[j] = 0.0;
+              }
+            }
+            else
+            {
+              // No linear acceleration available
+              realtime_pubs_[i]->msg_.linear_acceleration_covariance[0] = -1.0;
+            }
+          }
+          
+          realtime_pubs_[i]->unlockAndPublish();
+        }
+      }
+    }
+  }
+
+  void ImuSensorController::stopping(const ros::Time& time)
+  {}
+
+}
+
+
+PLUGINLIB_DECLARE_CLASS(imu_sensor_controller, ImuSensorController, imu_sensor_controller::ImuSensorController, controller_interface::ControllerBase)

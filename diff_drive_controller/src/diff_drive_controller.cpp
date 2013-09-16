147a471446e4625424e6958b294d5240f789625b
Bence Magyar <bence.magyar@pal-robotics.com>
2013-09-16 17:57:14 +0200

    Added odom message type + fixed loading problems of plugin.

--- diff_drive_controller/src/diff_drive_controller.cpp
+++ diff_drive_controller/src/diff_drive_controller.cpp
@@ -40,51 +40,96 @@
 
 #include <urdf_parser/urdf_parser.h>
 
-namespace controller_ns{
+#include <realtime_tools/realtime_buffer.h>
 
-class DiffDriveController : public controller_interface::Controller<hardware_interface::VelocityJointInterface>
-{
-public:
-  bool init(hardware_interface::VelocityJointInterface* hw,
-            ros::NodeHandle& root_nh,
-            ros::NodeHandle &controller_nh)
+namespace diff_drive_controller{
+
+  class DiffDriveController : public controller_interface::Controller<hardware_interface::VelocityJointInterface>
   {
-    // get joint name from the parameter server
-    std::string my_joint;
-    if (!controller_nh.getParam("joint", my_joint)){
-      ROS_ERROR("Could not find joint name");
-      return false;
+  public:
+    bool init(hardware_interface::VelocityJointInterface* hw,
+              ros::NodeHandle& root_nh,
+              ros::NodeHandle &controller_nh)
+    {
+      ROS_INFO("Init DiffDriveController.");
+      // get joint names from the parameter server
+
+//      if (!controller_nh.getParam("joints", joints))
+//      {
+//        ROS_ERROR("Could not find joint names");
+//        return false;
+//      }
+
+//      // parse robot description
+//      std::string model_param_name = "/robot_description";
+//      bool res = nh.hasParam(model_param_name);
+//      std::string robot_model_str="";
+//      if(!res || !nh.getParam(model_param_name,robot_model_str))
+//      {
+//        ROS_ERROR("Robot descripion couldn't be retrieved from param server.");
+//        return false;
+//      }
+
+//      boost::shared_ptr<urdf::ModelInterface> modelPtr = urdf::parseURDF(robot_model_str);
+//      std::string wheel_joint_name = "wheel_left_joint";
+//      boost::shared_ptr<const urdf::Joint> wheelJointPtr    = modelPtr->getJoint(wheel_joint_name);
+//      if(!wheelJointPtr.get())
+//      {
+//        PAL_ORO_ERROR_STREAM_TAG(  PAL_DEV_DEBUG_ON, PAL_FILE <<':'<< __LINE__ << ":failed: "<< wheel_joint_name <<
+//                                   " couldn't be retrieved from model description" << '\n');
+//        return false;
+//      }
+
+//      double wheel_radius = fabs(wheelJointPtr->parent_to_joint_origin_transform.position.z);
+//      double wheel_base   = 2.0 * fabs(wheelJointPtr->parent_to_joint_origin_transform.position.y);
+//      PAL_ORO_INFO_STREAM_TAG(  PAL_DEV_DEBUG_ON, "Odometry params : wheel base " << wheel_base << ", wheel radius " << wheel_radius);
+
+//      _odometryInfo.wheelsRadius[odometry::OdometryInfo::LEFT_WHEEL]  = wheel_radius;
+//      _odometryInfo.wheelsRadius[odometry::OdometryInfo::RIGHT_WHEEL] = wheel_radius;
+//      _odometryInfo.wheelBase = wheel_base;
+
+
+      // get the joint object to use in the realtime loop
+
+      left_wheel_joint_ = hw->getHandle("wheel_left_joint");  // throws on failure
+      right_wheel_joint_ = hw->getHandle("wheel_right_joint");  // throws on failure
+      return true;
     }
 
-    // get the joint object to use in the realtime loop
-    left_wheel_joint_ = hw->getHandle(my_joint);  // throws on failure
-    right_wheel_joint_ = hw->getHandle(my_joint);  // throws on failure
-    return true;
-  }
+    void update(const ros::Time& time, const ros::Duration& period)
+    {
+      const double vel = 4.0;
+      left_wheel_joint_.setCommand(vel);
+      right_wheel_joint_.setCommand(vel);
+      // do stuff coming from cmd_vel interface
 
-  void update(const ros::Time& time, const ros::Duration& period)
-  {
-    // do stuff coming from cmd_vel interface
+      // publish odom
+    }
 
-    // publish odom
-  }
+    void starting(const ros::Time& time)
+    {
+      // set velocity to 0
+    }
 
+    void stopping(const ros::Time& time)
+    {
+      // set velocity to 0
+    }
 
-  void starting(const ros::Time& time)
-  {
-    // set velocity to 0
-  }
+  private:
+    struct Commands
+      {
+        double position_; // Last commanded position
+        double velocity_; // Last commanded velocity
+      };
 
-  void stopping(const ros::Time& time)
-  {
-    // set velocity to 0
-  }
+    hardware_interface::JointHandle left_wheel_joint_;
+    hardware_interface::JointHandle right_wheel_joint_;
+    nav_msgs::Odometry odom;
+    boost::shared_ptr<const urdf::Joint> joint_urdf_;
+    realtime_tools::RealtimeBuffer<Commands> command_;
+    Commands command_struct_;
+  };
 
-private:
-  hardware_interface::JointHandle left_wheel_joint_;
-  hardware_interface::JointHandle right_wheel_joint_;
-  nav_msgs::Odometry odom;
-};
-
-PLUGINLIB_DECLARE_CLASS(package_name, DiffDriveController, controller_ns::DiffDriveController, controller_interface::ControllerBase);
+  PLUGINLIB_DECLARE_CLASS(diff_drive_controller, DiffDriveController, diff_drive_controller::DiffDriveController, controller_interface::ControllerBase);
 }//namespace

90f1ef3d15df6514f80345de65b5f9df29fa44b8
Igorec <kalevatykhia@gmail.com>
2014-03-06 21:31:17 +0400

    Add support for an joint interfaces are not inherited from JointHandle. Add JointTrajectoryController specification for SplineJointInterface.

--- joint_trajectory_controller/include/joint_trajectory_controller/hardware_interface_adapter.h
+++ joint_trajectory_controller/include/joint_trajectory_controller/hardware_interface_adapter.h
@@ -41,6 +41,7 @@
 
 #include <control_toolbox/pid.h>
 #include <hardware_interface/joint_command_interface.h>
+#include <hardware_interface/posvelacc_command_interface.h>
 
 /**
  * \brief Helper class to simplify integrating the JointTrajectoryController with different hardware interfaces.
@@ -55,7 +56,7 @@ template <class HardwareInterface, class State>
 class HardwareInterfaceAdapter
 {
 public:
-  bool init(std::vector<hardware_interface::JointHandle>& joint_handles, ros::NodeHandle& controller_nh)
+  bool init(std::vector<typename HardwareInterface::ResourceHandleType>& joint_handles, ros::NodeHandle& controller_nh)
   {
     return false;
   }
@@ -198,6 +199,44 @@ private:
   std::vector<PidPtr> pids_;
 
   std::vector<hardware_interface::JointHandle>* joint_handles_ptr_;
+};
+
+
+/**
+ * \brief Adapter for a spline-controlled hardware interface. Forwards desired positions as commands.
+ */
+template <class State>
+class HardwareInterfaceAdapter<hardware_interface::PosVelAccJointInterface, State>
+{
+public:
+  HardwareInterfaceAdapter() : joint_handles_ptr_(0) {}
+
+  bool init(std::vector<hardware_interface::PosVelAccJointHandle>& joint_handles, ros::NodeHandle& controller_nh)
+  {
+    // Store pointer to joint handles
+    joint_handles_ptr_ = &joint_handles;
+
+    return true;
+  }
+
+  void starting(const ros::Time& time) {}
+  void stopping(const ros::Time& time) {}
+
+  void updateCommand(const ros::Time&     /*time*/,
+                     const ros::Duration& /*period*/,
+                     const State&         desired_state,
+                     const State&         /*state_error*/)
+  {
+    // Forward desired position to command
+    const unsigned int n_joints = joint_handles_ptr_->size();
+    for (unsigned int i = 0; i < n_joints; ++i)
+    {
+      (*joint_handles_ptr_)[i].setCommand(desired_state.position[i], desired_state.velocity[i], desired_state.acceleration[i]);
+    }
+  }
+
+private:
+  std::vector<hardware_interface::PosVelAccJointHandle>* joint_handles_ptr_;
 };
 
 #endif // header guard

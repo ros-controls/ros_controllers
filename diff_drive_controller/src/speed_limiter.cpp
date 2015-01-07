1b77a08cd4fd5e393eeda96f466356b25c16dcda
Enrique Fernandez <enrique.fernandez.perdomo@gmail.com>
2015-01-08 00:06:39 +0100

    Limit jerk

--- diff_drive_controller/src/speed_limiter.cpp
+++ diff_drive_controller/src/speed_limiter.cpp
@@ -52,26 +52,33 @@ namespace diff_drive_controller
   SpeedLimiter::SpeedLimiter(
     bool has_velocity_limits,
     bool has_acceleration_limits,
+    bool has_jerk_limits,
     double min_velocity,
     double max_velocity,
     double min_acceleration,
-    double max_acceleration
+    double max_acceleration,
+    double min_jerk,
+    double max_jerk
   )
-  : has_velocity_limits(has_velocity_limits),
-    has_acceleration_limits(has_acceleration_limits),
-    min_velocity(min_velocity),
-    max_velocity(max_velocity),
-    min_acceleration(min_acceleration),
-    max_acceleration(max_acceleration)
+  : has_velocity_limits(has_velocity_limits)
+  , has_acceleration_limits(has_acceleration_limits)
+  , has_jerk_limits(has_jerk_limits)
+  , min_velocity(min_velocity)
+  , max_velocity(max_velocity)
+  , min_acceleration(min_acceleration)
+  , max_acceleration(max_acceleration)
+  , min_jerk(min_jerk)
+  , max_jerk(max_jerk)
   {
   }
 
-  double SpeedLimiter::limit(double& v, double v0, double dt)
+  double SpeedLimiter::limit(double& v, double v0, double v1, double dt)
   {
     const double tmp = v;
 
     limit_velocity(v);
     limit_acceleration(v, v0, dt);
+    limit_jerk(v, v0, v1, dt);
 
     return tmp != 0.0 ? v / tmp : 1.0;
   }
@@ -100,6 +107,28 @@ namespace diff_drive_controller
       const double dv = clamp(v - v0, dv_min, dv_max);
 
       v = v0 + dv;
+    }
+
+    return tmp != 0.0 ? v / tmp : 1.0;
+  }
+
+  double SpeedLimiter::limit_jerk(double& v, double v0, double v1, double dt)
+  {
+    const double tmp = v;
+
+    if (has_jerk_limits)
+    {
+      const double dv  = v  - v0;
+      const double dv0 = v0 - v1;
+
+      const double dt2 = dt * dt;
+
+      const double da_min = min_jerk * dt2;
+      const double da_max = max_jerk * dt2;
+
+      const double da = clamp(dv - dv0, da_min, da_max);
+
+      v = v0 + dv0 + da;
     }
 
     return tmp != 0.0 ? v / tmp : 1.0;

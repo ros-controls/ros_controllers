631bb4ea2f4656b32dfeca1020ca52d7edea9115
Adolfo Rodriguez Tsouroukdissian <adolfo.rodriguez@pal-robotics.com>
2015-06-25 17:56:02 +0200

    joint_state_controller: Add extra joints support
    Allow to optionally specify a set of extra joints for state publishing that
    are not contained in the JointStateInterface associated to the controller.
    
    The state of these joints can be specified via ROS parameters, and remains
    constant over time.

--- joint_state_controller/src/joint_state_controller.cpp
+++ joint_state_controller/src/joint_state_controller.cpp
@@ -29,19 +29,22 @@
  * Author: Wim Meeussen
  */
 
+#include <algorithm>
+#include <cstddef>
 
 #include "joint_state_controller/joint_state_controller.h"
-
-
 
 namespace joint_state_controller
 {
 
-  bool JointStateController::init(hardware_interface::JointStateInterface* hw, ros::NodeHandle &root_nh, ros::NodeHandle& controller_nh)
+  bool JointStateController::init(hardware_interface::JointStateInterface* hw,
+                                  ros::NodeHandle&                         root_nh,
+                                  ros::NodeHandle&                         controller_nh)
   {
     // get all joint names from the hardware interface
     const std::vector<std::string>& joint_names = hw->getNames();
-    for (unsigned i=0; i<joint_names.size(); i++)
+    num_hw_joints_ = joint_names.size();
+    for (unsigned i=0; i<num_hw_joints_; i++)
       ROS_DEBUG("Got joint %s", joint_names[i].c_str());
 
     // get publishing period
@@ -54,13 +57,14 @@ namespace joint_state_controller
     realtime_pub_.reset(new realtime_tools::RealtimePublisher<sensor_msgs::JointState>(root_nh, "joint_states", 4));
 
     // get joints and allocate message
-    for (unsigned i=0; i<joint_names.size(); i++){
+    for (unsigned i=0; i<num_hw_joints_; i++){
       joint_state_.push_back(hw->getHandle(joint_names[i]));
       realtime_pub_->msg_.name.push_back(joint_names[i]);
       realtime_pub_->msg_.position.push_back(0.0);
       realtime_pub_->msg_.velocity.push_back(0.0);
       realtime_pub_->msg_.effort.push_back(0.0);
     }
+    addExtraJoints(controller_nh, realtime_pub_->msg_);
 
     return true;
   }
@@ -81,9 +85,11 @@ namespace joint_state_controller
         // we're actually publishing, so increment time
         last_publish_time_ = last_publish_time_ + ros::Duration(1.0/publish_rate_);
 
-        // populate joint state message
+        // populate joint state message:
+        // - fill only joints that are present in the JointStateInterface, i.e. indices [0, num_hw_joints_)
+        // - leave unchanged extra joints, which have static values, i.e. indices from num_hw_joints_ onwards
         realtime_pub_->msg_.header.stamp = time;
-        for (unsigned i=0; i<joint_state_.size(); i++){
+        for (unsigned i=0; i<num_hw_joints_; i++){
           realtime_pub_->msg_.position[i] = joint_state_[i].getPosition();
           realtime_pub_->msg_.velocity[i] = joint_state_[i].getVelocity();
           realtime_pub_->msg_.effort[i] = joint_state_[i].getEffort();
@@ -96,7 +102,79 @@ namespace joint_state_controller
   void JointStateController::stopping(const ros::Time& time)
   {}
 
-}
+  void JointStateController::addExtraJoints(const ros::NodeHandle& nh, sensor_msgs::JointState& msg)
+  {
 
+    // Preconditions
+    XmlRpc::XmlRpcValue list;
+    if (!nh.getParam("extra_joints", list))
+    {
+      ROS_DEBUG("No extra joints specification found.");
+      return;
+    }
+
+    if (list.getType() != XmlRpc::XmlRpcValue::TypeArray)
+    {
+      ROS_ERROR("Extra joints specification is not an array. Ignoring.");
+      return;
+    }
+
+    for(std::size_t i = 0; i < list.size(); ++i)
+    {
+      if (list[i].getType() != XmlRpc::XmlRpcValue::TypeStruct)
+      {
+        ROS_ERROR_STREAM("Extra joint specification is not a struct, but rather '" << list[i].getType() <<
+                         "'. Ignoring.");
+        continue;
+      }
+
+      if (!list[i].hasMember("name"))
+      {
+        ROS_ERROR_STREAM("Extra joint does not specify name. Ignoring.");
+        continue;
+      }
+
+      const std::string name = list[i]["name"];
+      if (std::find(msg.name.begin(), msg.name.end(), name) != msg.name.end())
+      {
+        ROS_WARN_STREAM("Joint state interface already contains specified extra joint '" << name << "'.");
+        continue;
+      }
+
+      const bool has_pos = list[i].hasMember("position");
+      const bool has_vel = list[i].hasMember("velocity");
+      const bool has_eff = list[i].hasMember("effort");
+
+      const XmlRpc::XmlRpcValue::Type typeDouble = XmlRpc::XmlRpcValue::TypeDouble;
+      if (has_pos && list[i]["position"].getType() != typeDouble)
+      {
+        ROS_ERROR_STREAM("Extra joint '" << name << "' does not specify a valid default position. Ignoring.");
+        continue;
+      }
+      if (has_vel && list[i]["velocity"].getType() != typeDouble)
+      {
+        ROS_ERROR_STREAM("Extra joint '" << name << "' does not specify a valid default velocity. Ignoring.");
+        continue;
+      }
+      if (has_eff && list[i]["effort"].getType() != typeDouble)
+      {
+        ROS_ERROR_STREAM("Extra joint '" << name << "' does not specify a valid default effort. Ignoring.");
+        continue;
+      }
+
+      // State of extra joint
+      const double pos = has_pos ? static_cast<double>(list[i]["position"]) : 0.0;
+      const double vel = has_vel ? static_cast<double>(list[i]["velocity"]) : 0.0;
+      const double eff = has_eff ? static_cast<double>(list[i]["effort"])   : 0.0;
+
+      // Add extra joints to message
+      msg.name.push_back(name);
+      msg.position.push_back(pos);
+      msg.velocity.push_back(vel);
+      msg.effort.push_back(eff);
+    }
+  }
+
+}
 
 PLUGINLIB_EXPORT_CLASS( joint_state_controller::JointStateController, controller_interface::ControllerBase)
--- joint_state_controller/test/joint_state_controller_test.cpp
+++ joint_state_controller/test/joint_state_controller_test.cpp
@@ -44,7 +44,7 @@ class JointStateControllerTest : public ::testing::Test
 public:
   JointStateControllerTest()
     : root_nh_(ros::NodeHandle()),
-      controller_nh_("test/joint_state_controller")
+      controller_nh_("test_ok/joint_state_controller")
   {
     // Intialize raw joint state data
     names_.push_back("joint1"); names_.push_back("joint2");
@@ -90,7 +90,6 @@ protected:
     ++rec_msgs_;
   }
 };
-
 
 TEST_F(JointStateControllerTest, initOk)
 {
@@ -193,8 +192,119 @@ TEST_F(JointStateControllerTest, valuesOk)
   ASSERT_EQ(last_msg_.name.size(), last_msg_.effort.size());
 
   // Check payload values
-  typedef std::vector<std::string>::size_type      SizeType;
-  typedef std::vector<std::string>::iterator Iterator;
+  typedef std::vector<std::string>::size_type SizeType;
+  typedef std::vector<std::string>::iterator  Iterator;
+  for (SizeType raw_id = 0; raw_id < names_.size(); ++raw_id)
+  {
+    Iterator it = std::find(last_msg_.name.begin(), last_msg_.name.end(), names_[raw_id]);
+    ASSERT_NE(last_msg_.name.end(), it);
+    SizeType msg_id = std::distance(last_msg_.name.begin(), it);
+    EXPECT_EQ(pos_[raw_id], last_msg_.position[msg_id]);
+    EXPECT_EQ(vel_[raw_id], last_msg_.velocity[msg_id]);
+    EXPECT_EQ(eff_[raw_id], last_msg_.effort[msg_id]);
+  }
+  jsc.stopping(ros::Time::now());
+}
+
+TEST_F(JointStateControllerTest, extraJointsOk)
+{
+  JointStateController jsc;
+  ros::NodeHandle extra_joints_nh("test_extra_joints_ok/joint_state_controller");
+  EXPECT_TRUE(jsc.init(&js_iface_, root_nh_, extra_joints_nh));
+
+  XmlRpc::XmlRpcValue extra_joints;
+  ASSERT_TRUE(extra_joints_nh.getParam("extra_joints", extra_joints));
+  const int num_joints = names_.size() + extra_joints.size();
+
+  int pub_rate;
+  ASSERT_TRUE(controller_nh_.getParam("publish_rate", pub_rate));
+  ros::Duration period(1.0 / pub_rate);
+  jsc.starting(ros::Time::now());
+
+  pos_[0] = 1.0; pos_[1] = -1.0;
+  vel_[0] = 2.0; vel_[1] = -2.0;
+  eff_[0] = 3.0; eff_[1] = -3.0;
+
+  period.sleep();
+  jsc.update(ros::Time::now(), ros::Duration());
+  period.sleep(); ros::spinOnce(); // To trigger callback
+
+  // Check payload sizes
+  ASSERT_EQ(num_joints, last_msg_.name.size());
+  ASSERT_EQ(num_joints, last_msg_.position.size());
+  ASSERT_EQ(num_joints, last_msg_.velocity.size());
+  ASSERT_EQ(num_joints, last_msg_.effort.size());
+
+  // Check payload values: joint state interface resources
+  typedef std::vector<std::string>::size_type SizeType;
+  typedef std::vector<std::string>::iterator  Iterator;
+  for (SizeType raw_id = 0; raw_id < names_.size(); ++raw_id)
+  {
+    Iterator it = std::find(last_msg_.name.begin(), last_msg_.name.end(), names_[raw_id]);
+    ASSERT_NE(last_msg_.name.end(), it);
+    SizeType msg_id = std::distance(last_msg_.name.begin(), it);
+    EXPECT_EQ(pos_[raw_id], last_msg_.position[msg_id]);
+    EXPECT_EQ(vel_[raw_id], last_msg_.velocity[msg_id]);
+    EXPECT_EQ(eff_[raw_id], last_msg_.effort[msg_id]);
+  }
+
+  // Check payload values: Extra joints
+  {
+    Iterator it = std::find(last_msg_.name.begin(), last_msg_.name.end(), "extra1");
+    ASSERT_NE(last_msg_.name.end(), it);
+    SizeType msg_id = std::distance(last_msg_.name.begin(), it);
+    EXPECT_EQ(10.0, last_msg_.position[msg_id]);
+    EXPECT_EQ(20.0, last_msg_.velocity[msg_id]);
+    EXPECT_EQ(30.0, last_msg_.effort[msg_id]);
+  }
+  {
+    Iterator it = std::find(last_msg_.name.begin(), last_msg_.name.end(), "extra2");
+    ASSERT_NE(last_msg_.name.end(), it);
+    SizeType msg_id = std::distance(last_msg_.name.begin(), it);
+    EXPECT_EQ(-10.0, last_msg_.position[msg_id]);
+    EXPECT_EQ(  0.0, last_msg_.velocity[msg_id]);
+    EXPECT_EQ(  0.0, last_msg_.effort[msg_id]);
+  }
+  {
+    Iterator it = std::find(last_msg_.name.begin(), last_msg_.name.end(), "extra3");
+    ASSERT_NE(last_msg_.name.end(), it);
+    SizeType msg_id = std::distance(last_msg_.name.begin(), it);
+    EXPECT_EQ(0.0, last_msg_.position[msg_id]);
+    EXPECT_EQ(0.0, last_msg_.velocity[msg_id]);
+    EXPECT_EQ(0.0, last_msg_.effort[msg_id]);
+  }
+
+  jsc.stopping(ros::Time::now());
+}
+
+TEST_F(JointStateControllerTest, extraJointsKo)
+{
+  JointStateController jsc;
+  ros::NodeHandle extra_joints_nh("test_extra_joints_ko/joint_state_controller");
+  EXPECT_TRUE(jsc.init(&js_iface_, root_nh_, extra_joints_nh));
+
+  int pub_rate;
+  ASSERT_TRUE(controller_nh_.getParam("publish_rate", pub_rate));
+  ros::Duration period(1.0 / pub_rate);
+  jsc.starting(ros::Time::now());
+
+  pos_[0] = 1.0; pos_[1] = -1.0;
+  vel_[0] = 2.0; vel_[1] = -2.0;
+  eff_[0] = 3.0; eff_[1] = -3.0;
+
+  period.sleep();
+  jsc.update(ros::Time::now(), ros::Duration());
+  period.sleep(); ros::spinOnce(); // To trigger callback
+
+  // Check payload sizes
+  ASSERT_EQ(names_.size(),         last_msg_.name.size());
+  ASSERT_EQ(last_msg_.name.size(), last_msg_.position.size());
+  ASSERT_EQ(last_msg_.name.size(), last_msg_.velocity.size());
+  ASSERT_EQ(last_msg_.name.size(), last_msg_.effort.size());
+
+  // Check payload values: No extra joints should be present
+  typedef std::vector<std::string>::size_type SizeType;
+  typedef std::vector<std::string>::iterator  Iterator;
   for (SizeType raw_id = 0; raw_id < names_.size(); ++raw_id)
   {
     Iterator it = std::find(last_msg_.name.begin(), last_msg_.name.end(), names_[raw_id]);

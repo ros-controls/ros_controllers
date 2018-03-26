ba06c60349716d3d1af12296088034ad676be28d
Jeremie Deray <jeremie.deray@pal-robotics.com>
2018-03-26 11:06:02 +0200

    skidsteerbot publishes its own clock

--- diff_drive_controller/test/skidsteerbot.cpp
+++ diff_drive_controller/test/skidsteerbot.cpp
@@ -29,6 +29,7 @@
 
 // ROS
 #include <ros/ros.h>
+#include <rosgraph_msgs/Clock.h>
 
 // ros_control
 #include <controller_manager/controller_manager.h>
@@ -40,19 +41,54 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "skidsteerbot");
   ros::NodeHandle nh;
 
+  // This should be set in launch files
+  // as well
+  nh.setParam("/use_sim_time", true);
+
   Diffbot<6> robot;
   ROS_WARN_STREAM("period: " << robot.getPeriod().toSec());
   controller_manager::ControllerManager cm(&robot, nh);
 
-  ros::Rate rate(1.0 / robot.getPeriod().toSec());
+  ros::Publisher clock_publisher = nh.advertise<rosgraph_msgs::Clock>("/clock", 1);
+
+  //ros::Rate rate(1.0 / robot.getPeriod().toSec());
   ros::AsyncSpinner spinner(1);
   spinner.start();
+
+  boost::chrono::system_clock::time_point begin = boost::chrono::system_clock::now();
+  boost::chrono::system_clock::time_point end   = boost::chrono::system_clock::now();
+
+  ros::Time internal_time(0);
+  const ros::Duration dt = robot.getPeriod();
+  double elapsed_secs = 0;
+
   while(ros::ok())
   {
+    begin = boost::chrono::system_clock::now();
+
     robot.read();
-    cm.update(robot.getTime(), robot.getPeriod());
+    cm.update(internal_time, dt);
     robot.write();
-    rate.sleep();
+
+    end = boost::chrono::system_clock::now();
+
+    elapsed_secs = boost::chrono::duration_cast<boost::chrono::duration<double> >((end - begin)).count();
+
+    if (dt.toSec() - elapsed_secs < 0.0)
+    {
+      ROS_WARN_STREAM_THROTTLE(
+            0.1, "Control cycle is taking to much time, elapsed: " << elapsed_secs);
+    }
+    else
+    {
+      ROS_DEBUG_STREAM_THROTTLE(1.0, "Control cycle is, elapsed: " << elapsed_secs);
+      usleep((dt.toSec() - elapsed_secs) * 1e6);
+    }
+
+    rosgraph_msgs::Clock clock;
+    clock.clock = ros::Time(internal_time);
+    clock_publisher.publish(clock);
+    internal_time += dt;
   }
   spinner.stop();
 

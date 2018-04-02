f402c6dd78028d2f4ea84c5d0091d53e9cbd395e
Ryosuke Tajima <ryosuke.tajima@opensource-robotics.tokyo.jp>
2018-04-02 09:47:43 +0900

    Use a copy of the pointer in update()

--- joint_trajectory_controller/include/joint_trajectory_controller/joint_trajectory_controller_impl.h
+++ joint_trajectory_controller/include/joint_trajectory_controller/joint_trajectory_controller_impl.h
@@ -477,17 +477,17 @@ update(const ros::Time& time, const ros::Duration& period)
                                   desired_state_, state_error_);
 
   // Set action feedback
-  if (rt_active_goal_)
+  if (current_active_goal)
   {
-    rt_active_goal_->preallocated_feedback_->header.stamp          = time_data_.readFromRT()->time;
-    rt_active_goal_->preallocated_feedback_->desired.positions     = desired_state_.position;
-    rt_active_goal_->preallocated_feedback_->desired.velocities    = desired_state_.velocity;
-    rt_active_goal_->preallocated_feedback_->desired.accelerations = desired_state_.acceleration;
-    rt_active_goal_->preallocated_feedback_->actual.positions      = current_state_.position;
-    rt_active_goal_->preallocated_feedback_->actual.velocities     = current_state_.velocity;
-    rt_active_goal_->preallocated_feedback_->error.positions       = state_error_.position;
-    rt_active_goal_->preallocated_feedback_->error.velocities      = state_error_.velocity;
-    rt_active_goal_->setFeedback( rt_active_goal_->preallocated_feedback_ );
+    current_active_goal->preallocated_feedback_->header.stamp          = time_data_.readFromRT()->time;
+    current_active_goal->preallocated_feedback_->desired.positions     = desired_state_.position;
+    current_active_goal->preallocated_feedback_->desired.velocities    = desired_state_.velocity;
+    current_active_goal->preallocated_feedback_->desired.accelerations = desired_state_.acceleration;
+    current_active_goal->preallocated_feedback_->actual.positions      = current_state_.position;
+    current_active_goal->preallocated_feedback_->actual.velocities     = current_state_.velocity;
+    current_active_goal->preallocated_feedback_->error.positions       = state_error_.position;
+    current_active_goal->preallocated_feedback_->error.velocities      = state_error_.velocity;
+    current_active_goal->setFeedback( current_active_goal->preallocated_feedback_ );
   }
 
   // Publish state

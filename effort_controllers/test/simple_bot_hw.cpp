8eff8e4f0f9b9c5ea98812e398eb9a0ac6278161
Franco Fusco <francofusco93@gmail.com>
2020-04-10 10:11:33 +0200

    update test according to review

--- effort_controllers/test/simple_bot_hw.cpp
+++ effort_controllers/test/simple_bot_hw.cpp
@@ -1,36 +1,51 @@
-///////////////////////////////////////////////////////////////////////////////
-// Copyright (C) 2013, PAL Robotics S.L.
-//
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-//   * Redistributions of source code must retain the above copyright notice,
-//     this list of conditions and the following disclaimer.
-//   * Redistributions in binary form must reproduce the above copyright
-//     notice, this list of conditions and the following disclaimer in the
-//     documentation and/or other materials provided with the distribution.
-//   * Neither the name of PAL Robotics, Inc. nor the names of its
-//     contributors may be used to endorse or promote products derived from
-//     this software without specific prior written permission.
-//
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-// POSSIBILITY OF SUCH DAMAGE.
-//////////////////////////////////////////////////////////////////////////////
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2008, Willow Garage, Inc.
+ *  Copyright (c) 2012, hiDOF, Inc.
+ *  Copyright (c) 2020, Franco Fusco.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/*
+ Author: Franco Fusco
+*/
+
+#include <chrono>
+#include <cmath>
+#include <thread>
 
 // ROS
-#include <chrono>
-#include <thread>
 #include <ros/ros.h>
-#include <std_msgs/Float64.h>
 #include <rosgraph_msgs/Clock.h>
+#include <std_msgs/Float64.h>
 
 // ros_control
 #include <controller_manager/controller_manager.h>
@@ -45,10 +60,10 @@ public:
   static constexpr double period = 0.01;
 
   SimpleBot()
-  : pos_(0)
-  , vel_(0)
-  , eff_(0)
-  , max_eff_(10)
+  : pos_(0.0)
+  , vel_(0.0)
+  , eff_(0.0)
+  , max_eff_(10.0)
   {
     // joint state interface
     hardware_interface::JointStateHandle state_handle("joint", &pos_, &vel_, &eff_);
@@ -62,7 +77,7 @@ public:
 
   void read() {
     // update current state
-    pos_ += period * vel_ + 0.5*std::pow(period, 2.) * eff_;
+    pos_ += period * vel_ + 0.5*std::pow(period, 2.0) * eff_;
     vel_ += period * eff_;
   }
 
@@ -71,7 +86,7 @@ public:
     eff_ = cmd_;
     if(std::fabs(eff_) > max_eff_) {
       ROS_WARN("Large effort command received: %f; cutting down to % f", eff_, max_eff_);
-      eff_ = eff_ > 0 ? max_eff_ : -max_eff_;
+      eff_ = eff_ > 0.0 ? max_eff_ : -max_eff_;
     }
   }
 
@@ -94,9 +109,9 @@ int main(int argc, char **argv)
   ros::AsyncSpinner spinner(2);
   spinner.start();
 
-  ros::Rate rate(1./bot.period);
+  ros::Rate rate(1.0/bot.period);
   ros::Duration period(bot.period);
-  double sim_time = 0;
+  double sim_time = 0.0;
 
   // main "simulation"
   while(ros::ok()) {

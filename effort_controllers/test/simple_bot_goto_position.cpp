30eb1be4f595fe34e3b15f843e34be1c84668589
Franco Fusco <francofusco93@gmail.com>
2020-04-10 10:11:33 +0200

    update test according to review

--- effort_controllers/test/simple_bot_goto_position.cpp
+++ effort_controllers/test/simple_bot_goto_position.cpp
@@ -1,7 +1,47 @@
-#include <ros/ros.h>
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2008, Willow Garage, Inc.
+ *  Copyright (c) 2012, hiDOF, Inc.
+ *  Copyright (c) 2020, Franco Fusco.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/*
+ Author: Franco Fusco
+*/
+
 #include <gtest/gtest.h>
-#include <std_msgs/Float64.h>
+#include <ros/ros.h>
 #include <sensor_msgs/JointState.h>
+#include <std_msgs/Float64.h>
 
 
 class SimpleBotFixture : public ::testing::Test {
@@ -11,15 +51,13 @@ protected:
   ros::NodeHandle nh;
   double position;
 
-  int val;
-
   void positionCB(const sensor_msgs::JointState& msg) {
     position = msg.position[0];
   }
 
-  bool waitToReachTarget(double target, std::string& err, double precision=5e-2, double timeout=20) {
+  bool waitToReachTarget(double target, std::string& err, double precision=5e-2, double timeout=20.0) {
     position = 1e3*precision + target; // make position != target
-    ros::Rate rate(20);
+    ros::Rate rate(20.0);
     std_msgs::Float64 cmd;
     cmd.data = target;
     ros::Time start = ros::Time::now();
@@ -42,7 +80,7 @@ protected:
 
   void SetUp() override {
     // setup
-    position = 0;
+    position = 0.0;
     pub = nh.advertise<std_msgs::Float64>("position_controller/command", 1);
     sub = nh.subscribe("joint_states", 1, &SimpleBotFixture::positionCB, this);
   }

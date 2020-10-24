bbddcaf9e1b26057c8e040dac2ff00297a7858bb
Mateus Amarante Araújo <mateus.amarujo@gmail.com>
2020-10-24 09:19:15 -0300

    Format CMakeLists.txt and package.xml files + clean deps of 4ws_controller pkg

--- four_wheel_steering_controller/src/four_wheel_steering_controller.cpp
+++ four_wheel_steering_controller/src/four_wheel_steering_controller.cpp
@@ -35,6 +35,7 @@
 
 #include <cmath>
 #include <four_wheel_steering_controller/four_wheel_steering_controller.h>
+#include <pluginlib/class_list_macros.hpp>
 #include <tf/transform_datatypes.h>
 #include <urdf_geometry_parser/urdf_geometry_parser.h>
 
@@ -671,3 +672,5 @@ namespace four_wheel_steering_controller{
   }
 
 } // namespace four_wheel_steering_controller
+
+PLUGINLIB_EXPORT_CLASS(four_wheel_steering_controller::FourWheelSteeringController, controller_interface::ControllerBase)

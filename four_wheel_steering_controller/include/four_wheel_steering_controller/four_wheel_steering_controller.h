7dd4f6a33a6731f05ec48fc79e825a149ec8cabb
Mateus Amarante Araújo <mateus.amarujo@gmail.com>
2020-10-24 09:19:15 -0300

    Format CMakeLists.txt and package.xml files + clean deps of 4ws_controller pkg

--- four_wheel_steering_controller/include/four_wheel_steering_controller/four_wheel_steering_controller.h
+++ four_wheel_steering_controller/include/four_wheel_steering_controller/four_wheel_steering_controller.h
@@ -38,7 +38,6 @@
 
 #include <controller_interface/multi_interface_controller.h>
 #include <hardware_interface/joint_command_interface.h>
-#include <pluginlib/class_list_macros.hpp>
 
 #include <nav_msgs/Odometry.h>
 #include <four_wheel_steering_msgs/FourWheelSteeringStamped.h>
@@ -231,5 +230,4 @@ namespace four_wheel_steering_controller{
 
   };
 
-  PLUGINLIB_EXPORT_CLASS(four_wheel_steering_controller::FourWheelSteeringController, controller_interface::ControllerBase);
 } // namespace four_wheel_steering_controller
